051a6a870267b9176560051c970a9a66019b9b04
Jason Stallings <jason@stallin.gs>
2018-08-17 18:42:39 -0500

    feat: Allow picking a device.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 const adb = require('adbkit');
 const client = adb.createClient();
 const program = require('commander');
+const inquirer = require('inquirer');
 const pjson = require('./package.json');
 
 let ip;
@@ -21,20 +22,42 @@ program
   })
   .parse(process.argv);
 
-const sleep = (ms) => () => new Promise(resolve => setTimeout(resolve, 5000));
+const sleep = (ms) => () => new Promise(resolve => setTimeout(resolve, ms));
 
 client.listDevices()
 .then((devices) => {
   let device;
-  if (devices.length === 1 && !ip) {
-    device = devices[0];
+  if (devices.length != 0 && ip) {
+    const id = `${ip}:5555`;
+    // Loop for IP in list.
+    const filteredDevices = devices.filter(e => e.id === id);
+    if (filteredDevices.length > 0) {
+      return filteredDevices[0];
+    } else {
+      throw new Error('Device not found.');
+    }
+  // Only one device and no IP passed.
+  } else if (devices.length === 1 && !ip) {
+    return devices[0];
+  } else if (devices.length > 0 && !ip) {
+    return inquirer.prompt([{
+      type: 'list',
+      name: 'device',
+      message: 'Which Fire TV would you like to pair with?',
+      choices: devices.map(d => {
+        return {
+          name: d.id,
+          value: d.id
+        };
+      })
+    }])
+    .then((answers) => {
+      const id = answers['device'];
+      return devices.filter(device => { return device.id == id; })[0];
+    });
   }
-
-  return loop(Promise.resolve(), () => playIfNotPlaying(client, device).then(sleep(5000)));
 })
-.then(function() {
-  console.log('Done.')
-})
+.then(device => loop(Promise.resolve(), () => playIfNotPlaying(client, device).then(sleep(5000))))
 .catch(function(err) {
   console.error('Something went wrong:', err.stack)
 })
@@ -47,7 +70,10 @@ function loop(promise, fn) {
 
 const playIfNotPlaying = (client, device) => getPlayingState(client, device)
 .then(({ state, device }) => {
-  if (state !== 3) {
+  if (state === 0) {
+    console.log('Unknown state.');
+    return state;
+  } if (state !== 3) {
     console.log('Paused, pressing play.');
     return pressPlay(client, device)
     .then(() => state);
@@ -63,7 +89,11 @@ const getPlayingState = (client, device) => client.shell(device.id, 'dumpsys med
 .then(adb.util.readAll)
 .then(function(output) {
   const matches = /state=PlaybackState.*state=(\d)/gm.exec(output);
-  return { device, state: parseInt(matches[1]) };
+  let state = 0;
+  if (matches) {
+    state = parseInt(matches[1])
+  }
+  return { device, state };
 });
 
 const pressPlay = (client, device) => client.shell(device.id, 'input keyevent 85')
